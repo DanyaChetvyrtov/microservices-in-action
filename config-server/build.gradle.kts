3d878a164f4144fcc23eb28447d5be4ff89694f5
Danil <dabcom2013@gmail.com>
2025-06-12 22:28:58 +0300

    Configure vault with config-server

--- config-server/build.gradle.kts
+++ config-server/build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-config-server")
 
+//    implementation("org.springframework.vault:spring-vault-core")
+//    implementation("org.springframework.cloud:spring-cloud-starter-vault-config")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
