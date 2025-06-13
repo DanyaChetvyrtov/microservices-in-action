b19a9415f96d89d5ce6cceef0fca30297c7ff51c
Danil <dabcom2013@gmail.com>
2025-06-13 16:29:41 +0300

    Add clients to license ms

---
+++ license-ms/src/main/java/ru/danil/algos/ostock/service/client/OrganizationRestTemplateClient.java
@@ -0,0 +1,22 @@
+package ru.danil.algos.ostock.service.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import ru.danil.algos.ostock.model.Organization;
+
+@Component
+@RequiredArgsConstructor
+public class OrganizationRestTemplateClient {
+    private final RestTemplate restTemplate;
+
+    public Organization getOrganization(String organizationId) {
+        ResponseEntity<Organization> restExchange = restTemplate.exchange(
+                "http://organization-ms/v1/organization/{organizationId}",
+                HttpMethod.GET, null, Organization.class, organizationId
+        );
+        return restExchange.getBody();
+    }
+}
