348ceb50a4c30543b282aed815992f7934ed37af
Danil <dabcom2013@gmail.com>
2025-06-16 21:54:01 +0300

    Works. Configure & test role logic for organization

--- organization-ms/src/main/java/ru/danil/algos/organizationms/config/SecurityConfig.java
+++ organization-ms/src/main/java/ru/danil/algos/organizationms/config/SecurityConfig.java
@@ -1,31 +1,30 @@
 package ru.danil.algos.organizationms.config;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(jsr250Enabled = true)
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -46,9 +45,22 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthenticationConverter keycloakJwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-        converter.setAuthorityPrefix("ROLE_");
-        converter.setAuthoritiesClaimName("resource_access.ostock.roles");
+        Converter<Jwt, Collection<GrantedAuthority>> converter = jwt -> {
+            Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+            if (resourceAccess == null || !resourceAccess.containsKey("ostock")) {
+                return Collections.emptyList();
+            }
+
+            Map<String, Object> ostock = (Map<String, Object>) resourceAccess.get("ostock");
+            if (ostock == null || !ostock.containsKey("roles")) {
+                return Collections.emptyList();
+            }
+
+            Collection<String> roles = (Collection<String>) ostock.get("roles");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role)) // Добавляем префикс
+                    .collect(Collectors.toList());
+        };
 
         JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
@@ -64,5 +76,4 @@ public class SecurityConfig {
                 .jwsAlgorithm(SignatureAlgorithm.RS256)
                 .build();
     }
-
 }
--- organization-ms/src/main/java/ru/danil/algos/organizationms/controller/OrganizationController.java
+++ organization-ms/src/main/java/ru/danil/algos/organizationms/controller/OrganizationController.java
@@ -1,8 +1,11 @@
 package ru.danil.algos.organizationms.controller;
 
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.danil.algos.organizationms.mode.Organization;
 import ru.danil.algos.organizationms.service.OrganizationService;
@@ -14,24 +17,30 @@ public class OrganizationController {
     private final OrganizationService organizationService;
 
     @GetMapping("/{organizationId}")
-    public ResponseEntity<Organization> getOrganizationById(@PathVariable("organizationId") String organizationId) {
+    @RolesAllowed({"ADMIN", "USER"})
+//    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public ResponseEntity<Organization> getOrganizationById(@PathVariable("organizationId") String organizationId, Authentication authentication) {
+        System.out.println("Authorities: " + authentication.getAuthorities());
         return new ResponseEntity<>(organizationService.getById(organizationId), HttpStatus.OK);
     }
 
-    @PutMapping(value="/{organizationId}")
+    @PutMapping(value = "/{organizationId}")
+    @RolesAllowed({"ADMIN", "USER"})
     public ResponseEntity<Organization> updateOrganization(@PathVariable("organizationId") String id, @RequestBody Organization organization) {
         return ResponseEntity.ok(organizationService.update(organization));
     }
 
     @PostMapping
-    public ResponseEntity<Organization>  saveOrganization(@RequestBody Organization organization) {
+    @RolesAllowed({"ADMIN", "USER"})
+    public ResponseEntity<Organization> saveOrganization(@RequestBody Organization organization) {
         System.out.println(organization);
         return ResponseEntity.ok(organizationService.create(organization));
     }
 
-    @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/{organizationId}")
+    @RolesAllowed("ADMIN")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteOrganization( @PathVariable String organizationId) {
+    public void deleteOrganization(@PathVariable String organizationId) {
         organizationService.delete(organizationId);
     }
 }
